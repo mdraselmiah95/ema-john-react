f380d120ad66eb39f500ca7e24213156b85547eb
rasel <russelroy15@gmail.com>
2021-09-23 14:37:34 +0600

    WIP on ema-john simple website

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Shop.css";
 const Shop = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("./products.JSON")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
   return (
     <div className="shop-container">
       <div className="product-container">
