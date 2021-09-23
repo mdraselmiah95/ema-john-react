642adae27ae6cb2b5504407fcd543ff5fab00be2
rasel <russelroy15@gmail.com>
2021-09-23 17:08:55 +0600

    WIP on ema-john simple website

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,12 +9,40 @@ const Cart = (props) => {
   //   for (const product of cart) {
   //     total = total + product.price;
   //   }
+
+  const shipping = 15;
+  const tax = (total + shipping) * 10;
+  const grandTotal = total + shipping + tax;
   return (
     <div>
       <h3>Order summary</h3>
       <h5>Items Ordered: {props.cart.length}</h5>
       <br />
-      <p>Total: {total.toFixed(2)}</p>
+      {/* <p>Total: {total.toFixed(2)}</p> */}
+      <div>
+        <table style={{ width: "100%" }}>
+          <tr>
+            <td>Items: {props.cart.length}</td>
+            <td>$</td>
+          </tr>
+          <tr>
+            <td>Shipping & Handling:</td>
+            <td>${shipping.toFixed(2)}</td>
+          </tr>
+          <tr>
+            <td>Total before tax:</td>
+            <td>$</td>
+          </tr>
+          <tr>
+            <td>Estimated Tax:</td>
+            <td>$ {tax.toFixed(2)}</td>
+          </tr>
+          <tr>
+            <td>Order Total:</td>
+            <td>$ {grandTotal.toFixed(2)}</td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 };
