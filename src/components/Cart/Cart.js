2c522cd0dd647fa9a06369803cc65c53b80cf3ec
rasel <russelroy15@gmail.com>
2021-09-23 16:13:17 +0600

    WIP on ema-john simple website

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Cart.css";
 const Cart = (props) => {
+  //   console.log(props.cart);
+  const { cart } = props;
+  let total = 0;
+  for (const product of cart) {
+    total = total + product.price;
+  }
   return (
     <div>
       <h3>Order summary</h3>
       <h5>Items Ordered: {props.cart.length}</h5>
+      <br />
+      <p>Total: {total}</p>
     </div>
   );
 };
