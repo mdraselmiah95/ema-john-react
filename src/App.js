f1425c44aee3e7b586f17cab0aedc142efaa337a
rasel <russelroy15@gmail.com>
2021-09-23 14:04:27 +0600

    WIP on ema-john simple website

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div>
-      <h3>THis is resources</h3>
+      <Header />
     </div>
   );
 }
