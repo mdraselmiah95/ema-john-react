40a9d520f559f9f3a73874949aaaf5baf8b41941
rasel <russelroy15@gmail.com>
2021-09-23 14:14:04 +0600

    WIP on ema-john simple website

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,13 @@ import logo from "../../images/logo.png";
 import "./Header.css";
 const Header = () => {
   return (
-    <div>
-      <img src={logo} alt="logo" />
+    <div className="header">
+      <img className="logo" src={logo} alt="logo" />
+      <nav>
+        <a href="/shop">Shop</a>
+        <a href="/order">Order Review</a>
+        <a href="/inventory">Manage Inventory here</a>
+      </nav>
     </div>
   );
 };
