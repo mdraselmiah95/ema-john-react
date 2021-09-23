c51182d45ec15c7dd884e65f3274eca8c82a559a
rasel <russelroy15@gmail.com>
2021-09-23 14:31:16 +0600

    WIP on ema-john simple website

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,8 +2,13 @@ import React from "react";
 import "./Shop.css";
 const Shop = () => {
   return (
-    <div>
-      <h2>This is shop</h2>
+    <div className="shop-container">
+      <div className="product-container">
+        <h3>Product</h3>
+      </div>
+      <div className="cart-container">
+        <h3>Order summary</h3>
+      </div>
     </div>
   );
 };
