afc4c1df86669bc218f34c6f5bba02bbfe46cfe7
rasel <russelroy15@gmail.com>
2021-09-23 16:35:07 +0600

    WIP on ema-john simple website

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,8 +1,11 @@
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import "./Product.css";
 const Product = (props) => {
   //   console.log(props.product);
   const { name, category, stock, star, img, seller, price } = props.product;
+  const cartIcon = <FontAwesomeIcon icon={faShoppingCart} />;
   return (
     <div className="product">
       <div className="product-img">
@@ -21,7 +24,7 @@ const Product = (props) => {
           onClick={() => props.handleAddToCart(props.product)}
           className="regular-btn"
         >
-          ADD TO CART
+          {cartIcon} ADD TO CART
         </button>
       </div>
     </div>
